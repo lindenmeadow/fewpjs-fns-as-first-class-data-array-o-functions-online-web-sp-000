4541197fa351f9b827b6ac3294529470883bab44
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-24 14:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function leashDog(name, breed) {
 function walkToPark(name, breed) {
   console.log(`Walk to the park with ${name} the ${breed}`);
 }
+
+function throwFrisbee(name, breed) {
+  console.log(`Throw the frisbee for ${name} the ${breed}`);
+}
+
+function wakeDog(name, breed) {
+  console.log(`Wake ${name} the ${breed}`);
+}
