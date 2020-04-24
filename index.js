51d5df237ee471d41c3d131a722f7f35521d8e7e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-24 15:05:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${name} the ${breed}`);
+  console.log(`Wake ${name} the ${dogBreed}`);
 }
 
 function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${name} the ${breed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${name} the ${breed}`);
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${name} the ${breed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with ${name} the ${breed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 
 function unleashDog(dogName, dogBreed) {
-  console.log(`Unleash ${name} the ${breed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
